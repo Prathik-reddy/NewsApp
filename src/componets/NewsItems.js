b9a49858905cc648ce11c1d0cf130f64b4a90dba
Prathik Reddy <tvkp333555@gmail.com>
2021-09-11 12:42:17 +0530

    addded class based components and used loop for fetching data

--- src/componets/NewsItems.js
+++ src/componets/NewsItems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
     render() {
+       let {title,description,imgUrl,newsUrl} = this.props;
         return (
-            <div>
-                this is a new item
+            <div className="my-3">
+                <div className="card" style={{"width":"18rem"}}>
+                    <img src={imgUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">Read More</a>
+                    </div>
+                </div>
             </div>
         )
     }
