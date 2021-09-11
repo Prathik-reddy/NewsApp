561ca530bf700ca35cbfd6f6c446affea2256e0c
Prathik Reddy <tvkp333555@gmail.com>
2021-09-11 18:27:38 +0530

    added next and previous button

--- src/componets/News.js
+++ src/componets/News.js
@@ -7,17 +7,50 @@ export class News extends Component {
     this.state={
       articles :[],
       loading : false,
+      page : 1,
+
     };
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=7384ba886c19455f9098f3eed2fc650e";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=7384ba886c19455f9098f3eed2fc650e&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles:parsedData.articles});
+    this.setState({
+      articles:parsedData.articles,
+      totalResults: parsedData.totalResults
+
+    });
 
   }
 
+  handlePrevClick = async ()=>{
+    console.log("this is previous click");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7384ba886c19455f9098f3eed2fc650e&page=${this.state.page-1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      pages: this.state.page-1,
+      articles:parsedData.articles,
+
+    })
+  }
+  handleNextClick = async ()=>{
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+        alert("max page reaached");
+    }
+    else{
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7384ba886c19455f9098f3eed2fc650e&page=${this.state.page + 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(this.state.page);
+    this.setState({
+      page: this.state.page + 1,
+      articles:parsedData.articles
+    })
+  }
+
+  }
   render() {
     return (
         <div className="container my-2">
@@ -28,6 +61,10 @@ export class News extends Component {
                 <NewsItems  title={element.title?element.title.length>60?element.title.slice(0,60)+"...":element.title:" "} description={element.description?element.description.length>100?element.description.slice(0,80)+"...":element.description:" "} imgUrl={element.urlToImage} newsUrl={element.url}/>
               </div>
           })}
+          </div>
+          <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark"  onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
         </div>
     )
